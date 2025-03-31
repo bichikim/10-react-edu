9068f63a0731caa6049be595f3056fc5907b08ef
bichikim <bichi@live.co.kr>
2025-03-31 22:41:30 +0900

    버튼에 cursor-pointer 클래스를 추가하여 마우스 커서 스타일을 개선함

--- src/routes/mini-project/Main.tsx
+++ src/routes/mini-project/Main.tsx
@@ -11,8 +11,8 @@ export default function Main() {
     <div>
       <h1>Main Trending Movie {timeWindow}</h1>
       <div className='flex gap-2'>
-        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={() => setTimeWindow('day')}>Day</button>
-        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={() => setTimeWindow('week')}>Week</button>
+        <button className='bg-blue-500 text-white p-2 rounded-md cursor-pointer' onClick={() => setTimeWindow('day')}>Day</button>
+        <button className='bg-blue-500 text-white p-2 rounded-md cursor-pointer' onClick={() => setTimeWindow('week')}>Week</button>
       </div>
       <div>
         {trendingMovie.data?.data.results.map((movie) => (
