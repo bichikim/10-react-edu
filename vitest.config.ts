efb14627d7e8cfdcdce3fa445fdc6a369a47ac2e
bichikim <bichi@live.co.kr>
2025-04-03 16:00:41 +0900

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [react(), tailwindcss()] as any,
   resolve: {
     alias: {
